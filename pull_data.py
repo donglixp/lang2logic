eef9c70a10a8d804cc3cab0ac0986f47a8761cd6
Li Dong <donglixp@gmail.com>
2019-11-09 17:42:31 +0800

    Update pull_data.py

--- pull_data.py
+++ pull_data.py
@@ -7,7 +7,7 @@ Path('tmp').mkdir_p()
 for model_name in ('seq2seq','seq2tree'):
   for data_name in ('jobqueries','geoqueries','atis'):
     fn = '%s_%s.zip' % (model_name, data_name)
-    link = 'http://homepages.inf.ed.ac.uk/s1478528/lang2logic/' + fn
+    link = 'http://dong.li/lang2logic/' + fn
     with open('tmp/' + fn, 'wb') as f_out:
       f_out.write(urllib2.urlopen(link).read())
     with zipfile.ZipFile('tmp/' + fn) as zf:
